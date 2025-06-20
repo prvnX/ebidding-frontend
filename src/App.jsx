c6bd86b156e5f2fe1a462b2b7c10e3f925159de3
LalithraI <lalithraindupa2002@gmail.com>
2025-06-20 19:02:36 +0530

    feat: Implement user profile page with editable fields, bid history, and settings tabs

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AuctionHome from './pages/AuctionMan/home'
 import AddItem from './pages/AuctionMan/addItem'
 
 import { Routes,Route } from 'react-router-dom'
+import ProfilePage from './pages/profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,8 @@ function App() {
 
         <Route path="/AuctionMan/Home" element={<AuctionHome/> }/>
         <Route path="/AuctionMan/addItem" element={<AddItem/> }/>
+
+        <Route path="/profile" element={<ProfilePage />} />
 
       </Routes>
     </>
